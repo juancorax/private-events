dd1be3f2c9ff3d69a0419f3e26d63994a685f162
Juan Manuel Corigliano Giuliani <126042007+juancorax@users.noreply.github.com>
2024-10-26 15:26:00 -0300

    feat: display event details on #show view

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
 
   resources :users, only: [ :show ]
-  resources :events, only: [ :index, :new, :create ]
+  resources :events, only: [ :index, :show, :new, :create ]
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 

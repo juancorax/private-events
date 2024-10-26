784b9755e4eb7af817861cd43d782da4d8c64e03
Juan Manuel Corigliano Giuliani <126042007+juancorax@users.noreply.github.com>
2024-10-26 12:43:11 -0300

    feat: add 'creator_id' foreign key column

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2024_10_26_143514) do
+ActiveRecord::Schema[7.2].define(version: 2024_10_26_153539) do
   create_table "events", force: :cascade do |t|
     t.string "title"
     t.text "body"
@@ -18,6 +18,7 @@ ActiveRecord::Schema[7.2].define(version: 2024_10_26_143514) do
     t.string "location"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "creator_id"
   end
 
   create_table "users", force: :cascade do |t|

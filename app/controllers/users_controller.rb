591074747d49ff505f2a90d6c564fe3e3b8e6a59
Juan Manuel Corigliano Giuliani <126042007+juancorax@users.noreply.github.com>
2024-10-26 15:49:54 -0300

    feat: restrict access to #show

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+  before_action :authenticate_user!, only: [ :show ]
+
   def show
     @user_created_events = User.find(params[:id]).created_events
   end
